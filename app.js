0b63bf5075e0451e08d8b5e9967fa4346340e8dc
Your Name <you@example.com>
2018-07-29 17:58:36 +0200

    game is ready

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
-// const card1=document.querySelector('.card');
-// card1.addEventListener('click', function(){this.classList.add('open');});
-var moves=0;
-
 function start_game(){
-//randamowanie elementów
-const cards=["diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle", "bomb", "diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle", "bomb"];
-cards.sort(function(a, b){return 0.5 - Math.random()});
-console.log(cards);
+    //randam cards
+    const cards=["diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle", "bomb", "diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle", "bomb"];
+    cards.sort(function(a, b){return 0.5 - Math.random()});
 
-//randamowanie kard
-let deck="";
-for (let i=0; i<=15; i++){
-    deck+=`<li class="card">
-        <i class="fa fa-${cards[i]}"></i>
-    </li>`
-};
+    let deck="";
+    for (let i=0; i<=15; i++){
+        deck+=`<li class="card">
+            <i class="fa fa-${cards[i]}"></i>
+        </li>`
+    };//for loop
 
-moves=0;
+    const panel="<span class=\"stars\"></span><!--stars --><span class=\"moves\"></span> moves<span class=\"repeat\"><i class=\"fa fa-repeat repeat\"></i></span>";
 
-document.querySelector('.deck').innerHTML=deck;
-document.querySelector('.stars').innerHTML='<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>';
-document.querySelector('.moves').innerHTML=moves;
-}//start game
+    let moves=0;
 
-start_game();
+document.querySelector('.deck').innerHTML=deck;//fill the deck
+document.querySelector('.panel').innerHTML=panel;//fill the panel
+document.querySelector('.stars').innerHTML='<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>';//fill the stars
+document.querySelector('.moves').innerHTML=moves;//fill the moves
 
-//odkrywanie kart
-var hi = document.querySelectorAll(".card");
-
-[...hi].forEach(function(el) {
+//cards openning
+var cards2 = document.querySelectorAll(".card");
+[...cards2].forEach(function(el) {
     el.addEventListener('click', function(){this.classList.add('open');
 
-    var qw = document.querySelectorAll(".open");
+//matching or error
+var open = document.querySelectorAll(".open");
 
-//wykrywa czy para czy bład
-
-    if (qw.length==2) {
+    if (open.length==2) {
         moves+=1;
-        console.log(moves);
-        document.querySelector('.moves').innerHTML=moves;
+        document.querySelector('.moves').innerHTML=moves;//update moves
 
-        //dodawanie gwiazdek
+            //update stars
             if (moves==12){
                 document.querySelector(".stars").innerHTML='<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i>';
             }
@@ -51,69 +42,63 @@ var hi = document.querySelectorAll(".card");
                 document.querySelector(".stars").innerHTML='<i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i>';
             }
 
-
-            if (qw[0].firstElementChild.className==qw[1].firstElementChild.className){
-                console.log('para');
-
-                [...qw].forEach(function(el) {
+            //if matching
+            if (open[0].firstElementChild.className==open[1].firstElementChild.className){
+                [...open].forEach(function(el) {
                 el.classList.remove('open');
                 el.classList.add('match');
-                // el.classList.remove('match');
-                // el.classList.remove('error');
 
-                var end=document.querySelectorAll(".match");
-                    if (end.length==1){
-                        console.log("wygrałeś");
-                        setTimeout(function(){
-                            document.querySelector('.deck').remove();
-                            document.querySelector('.panel').remove();
-                            const element=document.createElement('div');
-                            element.setAttribute("class", "game_end")
-                            const text=`<div class="icon"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
-                                        <div class="con">Congratulations! You've won!</div>
-                                        <div class="moves_score">${moves} moves were needed!</div>
-                                        <button class="button">Play again</button>`;
-                            element.innerHTML=text;
-                            document.querySelector('main').appendChild(element);}, 1000);//setTime
+            //ending the game
+            var end=document.querySelectorAll(".match");
+            if (end.length==16){
+                setTimeout(function(){
+                document.querySelector('.deck').remove();
+                document.querySelector('.panel').innerHTML="";
+                const element=document.createElement('div');
+                element.setAttribute("class", "game_end")
+                const text=`<div class="icon"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                            <div class="con">Congratulations! You've won!</div>
+                            <div class="moves_score">${moves} moves were needed!</div>
+                            <button class="button">Play again</button>`;
+                element.innerHTML=text;
+                document.querySelector('main').appendChild(element);
+
+                //EventListener for button
+                document.querySelector('.button').addEventListener('click',function(){
+                console.log("działa");
+                document.querySelector('.game_end').remove();
+                const elementDeck=document.createElement('div');
+                elementDeck.setAttribute("class", "deck");
+                document.querySelector('main').appendChild(elementDeck);
+                start_game();
+                });//addEventListener click button
+                }, 1000);//setTime
 
 
-                    }
-
-
+            }//if end.length
             });
 
             }
+            //if error
             else {
-                console.log('błąd');
-                [...qw].forEach(function(el) {
+                [...open].forEach(function(el) {
                 el.classList.remove('open');
                 el.classList.add('error');
 
                 // el.classList.remove('match');
                 setTimeout(function(){el.classList.remove('error')}, 1000);
-            });
+            });//forEach
 
-            }
-    }
-});
-});
+            }//else
+    }//if open.length==2
+    });//addEventListener click card
+});//cards2 forEach
 
-//kończenie gry
-
-//obsługa button - tu może brakowa deck albo jakoś tak
-// document.querySelector('.button').addEventListener('click',function(){
-// start_game();
-// });
-
-//obsługa repeat
-document.querySelector('.repeat').addEventListener('click',function(){
+//repeat button
+document.querySelector('.repeat').addEventListener('click', function(){
 start_game();
-});
+});//addListener repeat
+}//start game
 
-
-
-// document.querySelector('.card').firstElementChild.className; --pobiera klasę z i
-
-
-
-console.log('wypchaj się');
+//start the game
+start_game();
